2b7af688a238aa3a44cfaf4c969ea44353956163
jbwong05 <34504936+jbwong05@users.noreply.github.com>
2018-03-20 22:47:35 -0700

    Added Parametric Formatter Form

---
+++ Character Curves/Parametric Formatter Form/Parametric Formatter Form/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Parametric_Formatter_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Equation:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(622, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy Formatted Equation to Clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 99);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Parametric Equation Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Character Curves/Parametric Formatter Form/Parametric Formatter Form/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parametric_Formatter_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                Clipboard.SetText(formatEquation(textBox1.Text));
+            }
+        }
+
+        private string formatEquation(string originalEquation)
+        {
+            int n;
+            string formattedEquation = "";
+            while (originalEquation.IndexOf(" ") != -1)
+            {
+                formattedEquation += originalEquation.Substring(0, originalEquation.IndexOf(" "));
+                originalEquation = originalEquation.Substring(originalEquation.IndexOf(" ") + 1);
+                /*if (text.Substring(0, 1) == "s")
+                {
+                    formattedText += "*";
+                }*/
+            }
+            originalEquation = formattedEquation + originalEquation;
+            formattedEquation = "";
+            int temp = originalEquation.IndexOf("/");
+            while (temp != -1)
+            {
+                int firstBracket = temp - 1;
+                while (firstBracket >= 0 && int.TryParse(originalEquation.Substring(firstBracket, 1), out n))
+                {
+                    firstBracket--;
+                }
+                if (firstBracket == -1)
+                {
+                    formattedEquation += "(" + originalEquation.Substring(0, temp + 1);
+                }
+                else
+                {
+                    formattedEquation += originalEquation.Substring(0, firstBracket + 1) + "(" + originalEquation.Substring(firstBracket + 1, temp - firstBracket);
+                }
+                originalEquation = originalEquation.Substring(temp + 1);
+                int secondBracket = 0;
+                while (secondBracket < originalEquation.Length && int.TryParse(originalEquation.Substring(secondBracket, 1), out n))
+                {
+                    secondBracket++;
+                }
+                if (secondBracket >= originalEquation.Length)
+                {
+                    formattedEquation += originalEquation + ")";
+                }
+                else
+                {
+                    formattedEquation += originalEquation.Substring(0, secondBracket) + ")";
+                    originalEquation = originalEquation.Substring(secondBracket);
+                }
+                temp = originalEquation.IndexOf("/");
+            }
+            return (formattedEquation + originalEquation).Replace('t', 'T');
+        }
+    }
+}
